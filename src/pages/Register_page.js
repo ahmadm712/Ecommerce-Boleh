7e73200774e40a8bc120728098ea2e0ac6cca57c
Rafly Ramadhani Putra <raflyramadhaniputra28@gmail.com>
2021-11-14 13:51:48 +0700

    validasi form login dan register CLEAR

--- src/pages/Register_page.js
+++ src/pages/Register_page.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useHistory } from "react-router-dom";
 import axios from "axios";
 import Logo from "../assets/images/Shopping.png";
@@ -17,8 +17,15 @@ export default function RegisterPage() {
 	    phone: ""
 	})
 
+  const [dataErrors, setDataErrors] = useState({})
+   const [isSubmit, setIsSubmit] = useState(false);
+
 	function submit(e){
 		e.preventDefault();
+    setDataErrors(validate(data));
+    setIsSubmit(true);
+
+
 		axios.post(url, {
 			name: data.name,
 		    email: data.email,
@@ -39,6 +46,55 @@ export default function RegisterPage() {
 		setData(newData)
 		console.log(newData)
 	}
+
+
+  useEffect(() => {
+    console.log(dataErrors);
+    if(Object.keys(dataErrors).length === 0 && isSubmit) {
+      console.log(data);
+    }
+  },[dataErrors])
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+     
+
+     
+  
+    
+    if(!values.email) {
+        errors.email = "Email harus diisi!";
+      } else if (!regex.test(values.email)) {
+        errors.email = "Masukkan email yang valid!";
+      }
+  
+      if(!values.password) {
+        errors.password = "Password harus diisi!";    
+      }else if ((values.password.length < 8)) {
+        errors.password = "Password harus berisi minimal 8 karakter!"
+      }
+
+      if(!values.image) {
+        errors.image = "Gambar harus diisi!";
+      }
+
+      if(!values.saldo) {
+        errors.saldo = "Masukkan saldo!";
+      }
+      
+      if(!values.address) {
+        errors.address = "Alamat harus diisi!";
+      }
+
+      if(!values.phone) {
+        errors.phone = "Nomor harus diisi!";
+      }
+
+      return errors; 
+  };
+  
+
+
     return (
         <div className="min-h-screen flex justify-around px-54 py-12 bg-gradient-to-r from-primary-100 to-gray-50">
       
@@ -53,6 +109,9 @@ export default function RegisterPage() {
         
        </div>
           <div className="max-w-xl w-96 h-auto mx-auto mt-4 bg-white p-8 border border-gray-300 shadow-xl rounded-xl">
+
+         
+
             <form onSubmit={(e)=> submit(e)} className="space-y-6">
             <div class="h-14 w-14 mx-auto">
           <img src={Logo2} alt="Logo"/>
@@ -61,37 +120,62 @@ export default function RegisterPage() {
         <div className="text-3xl font-bold text-gray-900 mt-2 text-center">Register</div>
             <div>
               <label htmlFor="" className="text-sm font-bold text-gray-600 block">Full Name</label>
-              <input onChange={(e)=>handle(e)} id="name" value={data.name} placeholder="Input name" type="text" className="w-full p-2 border border-gray-300 rounded mt-1" required/>
+              <input onChange={(e)=>handle(e)} id="name" value={data.name} placeholder="Input name" type="text" className="w-full p-2 border border-gray-300 rounded mt-1"/>
             </div>
+            
+           
+
 
             <div>
               <label htmlFor="" className="text-sm font-bold text-gray-600 block">Email</label>
-              <input onChange={(e)=>handle(e)} id="email" value={data.email} placeholder="Input email" type="text" className="w-full p-2 border border-gray-300 rounded mt-1" required/>
+              <input onChange={(e)=>handle(e)} id="email" value={data.email} placeholder="Input email" type="text" className="w-full p-2 border border-gray-300 rounded mt-1"/>
+            </div>
+
+            <div className="text-red-600">
+            <p>{dataErrors.email}</p>
             </div>
 
             <div>
               <label htmlFor="" className="text-sm font-bold text-gray-600 block">Password</label>
-              <input onChange={(e)=>handle(e)} id="password" value={data.password} placeholder="Input password" type="text" className="w-full p-2 border border-gray-300 rounded mt-1" required/>
+              <input onChange={(e)=>handle(e)} id="password" value={data.password} placeholder="Input password" type="text" className="w-full p-2 border border-gray-300 rounded mt-1"/>
+            </div>
+
+            <div className="text-red-600">
+            <p>{dataErrors.password}</p>
             </div>
 
             <div>
               <label htmlFor="" className="text-sm font-bold text-gray-600 block">Image</label>
-              <input onChange={(e)=>handle(e)} id="image" value={data.image} placeholder="Input image" type="text" className="w-full p-2 border border-gray-300 rounded mt-1" required/>
+              <input onChange={(e)=>handle(e)} id="image" value={data.image} placeholder="Input image" type="text" className="w-full p-2 border border-gray-300 rounded mt-1"/>
+            </div>
+
+            <div className="text-red-600">
+            <p>{dataErrors.image}</p>
             </div>
 
             <div>
               <label htmlFor="" className="text-sm font-bold text-gray-600 block">Saldo</label>
-              <input onChange={(e)=>handle(e)} id="saldo" value={data.saldo} placeholder="Input saldo" type="text" className="w-full p-2 border border-gray-300 rounded mt-1" required/>
+              <input onChange={(e)=>handle(e)} id="saldo" value={data.saldo} placeholder="Input saldo" type="text" className="w-full p-2 border border-gray-300 rounded mt-1"/>
+            </div>
+
+            <div className="text-red-600">
+            <p>{dataErrors.saldo}</p>
             </div>
 
             <div>
               <label htmlFor="" className="text-sm font-bold text-gray-600 block">Address</label>
-              <input onChange={(e)=>handle(e)} id="address" value={data.address} placeholder="Input address" type="text" className="w-full p-2 border border-gray-300 rounded mt-1" required/>
+              <input onChange={(e)=>handle(e)} id="address" value={data.address} placeholder="Input address" type="text" className="w-full p-2 border border-gray-300 rounded mt-1"/>
+            </div>
+            <div className="text-red-600">
+            <p>{dataErrors.address}</p>
             </div>
 
             <div>
               <label htmlFor="" className="text-sm font-bold text-gray-600 block">Phone</label>
-              <input onChange={(e)=>handle(e)} id="phone" value={data.phone} placeholder="Input phone" type="text" className="w-full p-2 border border-gray-300 rounded mt-1" required/>
+              <input onChange={(e)=>handle(e)} id="phone" value={data.phone} placeholder="Input phone" type="text" className="w-full p-2 border border-gray-300 rounded mt-1"/>
+            </div>
+            <div className="text-red-600">
+            <p>{dataErrors.phone}</p>
             </div>
             	
             	<div class="mb-6 text-center">
